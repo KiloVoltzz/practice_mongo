f6c162051c5443c06649b172244eff0e6fa6005b
KiloVoltzz <abhishekkv001@gmail.com>
2025-08-17 11:51:33 +0530

    second day

--- backend/app.js
+++ backend/app.js
@@ -1,74 +1,38 @@
 const express = require("express");
-const path = require("path");
+// const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
 const app = express();
-const userModel = require("./userModels");
+var jwt = require("jsonwebtoken");
 
 //parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/create", async (req, res) => {
-  try {
-    const newUser = await userModel.create({
-      name: "rock",
-      email: "rock@gmail.com",
-      userName: "rockkkkkk",
-    });
-    res.status(201).send(newUser);
-    console.log("New user created");
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send({ message: "An error occurred.", error: error.message });
-  }
+app.get("/", (req, res) => {
+  // res.cookie("name","bob")
+  //   bcrypt.genSalt(10, function (err, salt) {
+  //     bcrypt.hash("lololo", salt, function (err, hash) {
+  //       // Store hash in your password DB.
+  //       console.log(hash);
+  //     });
+  //   });
+  var token = jwt.sign({ email: "bar@gmail.com" }, "shhhhh");
+  res.cookie("token", token);
+  res.send("done");
 });
 
-app.get("/update", async (req, res) => {
-  try {
-    let updateUser = await userModel.findOneAndUpdate(
-      { name: "rock", },
-      { userName: "rocks" },
-      { new: true }
-    );
-    res.status(201).send(updateUser);
-    console.log("user updated");
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send({ message: "An error occurred.", error: error.message });
-  }
+app.get("/read", (req, res) => {
+//   console.log(req.cookies);
+  //   bcrypt.compare("llololo", "$2b$10$fjfQRYHsyBv4lXTpl5Fbl.oED6HUzcQr2zQIXDsIndQWqesw.Gfj2", function (err, result) {
+  //     // result == true
+  //     console.log(result)
+  //   });
+  let data = jwt.verify(req.cookies.token, "shhhhh")
+  console.log(data); // bar
+  res.send("checking");
 });
 
-app.get("/read", async (req, res) => {
-  try {
-    let readUser = await userModel.find()
-    res.status(201).send(readUser);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send({ message: "An error occurred.", error: error.message });
-  }
-});
-
-app.get("/delete", async (req, res) => {
-  try {
-    let readUser = await userModel.findOneAndDelete({userName:"rocks"})
-    res.status(201).send(readUser);
-    console.log("user deleted");
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send({ message: "An error occurred.", error: error.message });
-  }
-});
-
-app.listen(5000, () => {
-  console.log("server is running");
+app.listen("3000", () => {
+  console.log("Server is running");
 });
