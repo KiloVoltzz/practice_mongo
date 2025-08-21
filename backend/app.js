a1336fff26e87efcd3eba380190ddfd2bba9ae6b
KiloVoltzz <abhishekkv001@gmail.com>
2025-08-21 11:58:06 +0530

    fourth day

--- backend/app.js
+++ backend/app.js
@@ -1,40 +1,38 @@
 const express = require("express");
-const path = require("path");
-const bcrypt = require("bcrypt");
-var jwt = require("jsonwebtoken");
-var cookieParser = require("cookie-parser");
-const User_model = require("./models/user_model");
-
-//middlewares
 const app = express();
+const userModel = require("./models/user");
+const postModel = require("./models/post");
 
-//parsers
-app.use(express.json());
+app.use(express());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.send("hello");
 });
 
-app.post("/create", async (req, res) => {
-  try {
-    let { name, email, password } = req.body;
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const newApp = await User_model.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-    res.json(newApp);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+app.get("/create", async (req, res) => {
+  let newUser = await userModel.create({
+    userName: "rob",
+    email: "rob@gmail.com",
+    age: 22,
+    posts: [],
+  });
+  res.send(newUser);
 });
 
-app.listen("3000", () => {
-  console.log("server is running");
+app.get("/newPost", async (req, res) => {
+  let newPost = await postModel.create({
+    postdata: "readddddd",
+    user: "68a55c6835c988d086bfee1d",
+  });
+
+  let user = await userModel.findOne({ _id: "68a55c6835c988d086bfee1d" });
+//   console.log(newPost)
+  user.posts.push(newPost._id);
+  user.save()
+  res.send(newPost);
+});
+
+app.listen(3000, () => {
+  console.log("servere is running");
 });
