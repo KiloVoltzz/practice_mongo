8ad17bd2f4f2dae00bf1f3e95fb86949ee8d2676
KiloVoltzz <abhishekkv001@gmail.com>
2025-09-07 16:09:32 +0530

    new practice

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
 
-// Middleware
+
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const upload = multer();
 
-// Routes
-const userRoutes = require("./routes/userRoutes");
-app.use("/api/users", userRoutes);
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
-module.exports = app;
+app.post("/test", upload.none(), (req, res) => {
+  console.log(req.body);
+  res.send("user data found");
+});
+
+app.listen(3000, () => {
+  console.log("server is running");
+});
